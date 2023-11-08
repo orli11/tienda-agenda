ed488742aec42e3d53510fc47ec1770753a58ab2
ximena <xsmg06@outlook.com>
2023-11-07 21:20:28 -0600

    PruebaaXimena

--- js/calendario.js
+++ js/calendario.js
@@ -67,22 +67,7 @@ month_picker.onclick = () => {
     month_list.classList.add('show')
 }
 
-let currDate = new Date()
 
-let curr_month = {value: currDate.getMonth()}
-let curr_year = {value: currDate.getFullYear()}
-
-generateCalendar(curr_month.value, curr_year.value)
-
-document.querySelector('#prev-year').onclick = () => {
-    --curr_year.value
-    generateCalendar(curr_month.value, curr_year.value)
-}
-
-document.querySelector('#next-year').onclick = () => {
-    ++curr_year.value
-    generateCalendar(curr_month.value, curr_year.value)
-}
 
 // Codigo de prueba
 //Hola, esto es una prueba

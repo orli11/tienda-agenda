51f1df0a5b18c6f1f19ed1c105a4ba8e3d69cfdd
Orlando Javier <oj.flores118@gmail.com>
2023-11-12 16:46:12 -0600

    Alerta para tiendaLogin, avances diseños, etc

--- js/tienda.js
+++ js/tienda.js
@@ -8,9 +8,20 @@ btnLogin.addEventListener('click', () => {
     if(email.value.trim() === '' || password.value.trim() === ''){
         //Alerta
         console.log('Todos los campos son obligatorios')
-        
+        activarAlerta('Todos los campos son obligatorios')
     } else {
         console.log('Usuario registrado')
     }
 })
 
+//Alerta 
+const activarAlerta = mensaje => {
+    const alert = document.getElementsByClassName('alert')
+    alert[0].textContent = mensaje
+    alert[0].classList.remove('hiden')
+    alert[0].classList.add('show')
+    setTimeout(() => {
+        alert[0].classList.remove('show')
+        alert[0].classList.add('hide')
+    }, 3000);
+}

2dd1d63ffa373667bbdf68fbeb55edb773612fc4
CastroPerla <perlacass936@gmail.com>
2023-11-27 11:34:54 -0600

    Cambios de tienda

--- js/tiendaRegistro.js
+++ js/tiendaRegistro.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const url = window.location.search
+    const params = new URLSearchParams(url)
+    const existe = params.get('existe')
+    if(existe){
+      const alerta = document.getElementsByClassName('alert')
+      alerta[0].classList.remove('hide')
+      alerta[0].classList.add('show')
+      setTimeout(() => {
+        alerta[0].classList.remove('show')
+        alerta[0].classList.add('hide')
+      }, 3000)
+    }
+    console.log('@@@ params => ', params, existe)
+  })
+
+/*
 const checkVendedor = document.getElementById('checkVendedor')
 const checkCliente = document.getElementById('checkCliente')
 
@@ -12,4 +29,4 @@ const redirigirSiElige = (check, pagina) => {
     if (check.checked) {
         window.location.href = pagina;
     }
-}
+} */ 

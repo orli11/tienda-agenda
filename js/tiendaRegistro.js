230887229ef03b54c870bad4a35babc855528e35
ximena <xsmg06@outlook.com>
2023-12-01 20:56:57 -0600

    intento registro login

--- js/tiendaRegistro.js
+++ js/tiendaRegistro.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const btnLogin = document.getElementById('btnLogin');
-    btnLogin.addEventListener('click', register);
 
     const url = window.location.search
     const params = new URLSearchParams(url)
@@ -16,40 +14,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     console.log('@@@ params => ', params, existe)
   })
-
-
-const checkVendedor = document.getElementById('checkVendedor')
-const checkCliente = document.getElementById('checkCliente')
-    register = () => {
-        // Obtener valores del formulario
-        const name = document.getElementById('name').value;
-        const apaterno = document.getElementById('apaterno').value;
-        const amaterno = document.getElementById('amaterno').value;
-        const tell = document.getElementById('tell').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        const checkVendedor = document.getElementById('checkVendedor').checked;
-        const checkCliente = document.getElementById('checkCliente').checked;
-
-        const sendData = {
-            name,
-            apaterno,
-            amaterno,
-            tell,
-            email,
-            password,
-            checkVendedor,
-            checkCliente
-        }
-
-        fetch('', {
-            method: 'POST',
-            body: JSON.stringify(sendData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then((res) => { 
-
-        })
-    }
